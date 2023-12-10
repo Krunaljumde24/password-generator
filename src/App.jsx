528bffdfcd5278b6dcfaf434ea396143a71afd4a
Krunal Jumde <KrunalJumde24@gmail.com>
2023-12-10 15:13:41 +0530

    Password generator app

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
+import { useState, useEffect, useCallback } from 'react'
 import './App.css'
-import { useCallback, useEffect, useState } from 'react'
 
 function App() {
 
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState('password')
   const [length, setLength] = useState(8)
-  const [numberAllowed, setNumberAllowed] = useState(false);
-  const [charactersAllowed, setCharactersAllowed] = useState(false)
+  const [allowNumbers, setAllowNumbers] = useState(false)
+  const [allowCharacters, setAllowCharacters] = useState(false)
+
+  let generatePassword = useCallback(() => {
+    let str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    let num = '0123456789';
+    let chars = '~!@#$%^&*()<>?:"{}|';
+    if (allowNumbers) {
+      str += num;
+    }
+    if (allowCharacters) {
+      str += chars;
+    }
 
 
-  let generatePassword = useCallback(
-    () => {
-      let aplphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-      let pass = '';
+    let pass = '';
+    for (let i = 0; i < length; i++) {
+      let index = Math.floor(Math.random() * str.length + 1);
+      pass = pass + str.charAt(index);
+    }
+    setPassword(pass);
+  }, [length, allowNumbers, allowCharacters])
 
-      for (let i = 0; i < length; i++) {
-        let index = Math.floor(Math.random() * aplphabets.length + 1);
-        pass += aplphabets.charAt(index)
-      }
-      setPassword(pass)
-    },
-    [length],
-  )
-
-  useEffect(generatePassword, [length])
-
+  useEffect(generatePassword, [length, allowNumbers, allowCharacters])
 
   return (
     <>
@@ -32,17 +36,67 @@ function App() {
         <h4>Password Generator</h4>
         <div className='row'>
           <div className="col-10">
-            <input type="text" className="form-control" name='password' value={password} />
+            <input
+              type="text"
+              className="form-control"
+              name='password'
+              value={password}
+              onChange={(event) => {
+                setPassword(event.target.value)
+              }}
+            />
           </div>
           <div className='col'>
-            <button type='button' className='btn btn-primary' onClick={generatePassword}>Copy</button>
+            <button
+              type='button'
+              className='btn btn-primary'>
+              Copy
+            </button>
           </div>
         </div>
         <br />
         <div className='second'>
-          <input type="range" id='range' min="5" max="50" value={length} /> <label htmlFor="range">Length {length}</label>
-          <input type="checkbox" id='num' /> <label htmlFor="num">Numbers</label>
-          <input type="checkbox" id='char' /> <label htmlFor="char">Characters</label>
+          <input
+            type="range"
+            id='range'
+            min="5"
+            max="50"
+            value={length}
+            onChange={(event) => {
+              setLength(event.target.value)
+            }}
+          />
+          <label
+            htmlFor="range"
+          >Length ({length})
+          </label>
+          <input
+            type="checkbox"
+            id='num'
+            value={allowNumbers}
+            onChange={() => {
+              setAllowNumbers((prev) => !prev)
+              console.log(allowNumbers);
+            }}
+          />
+          <label
+            htmlFor="num"
+          >
+            Numbers
+          </label>
+          <input
+            type="checkbox"
+            id='char'
+            value={allowCharacters}
+            onChange={() => {
+              setAllowCharacters((prev) => !prev)
+            }}
+          />
+          <label
+            htmlFor="char"
+          >
+            Characters
+          </label>
         </div>
       </div>
     </>
